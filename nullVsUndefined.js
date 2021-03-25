859b169022caffe92e2579f2a0c81e203533c01d
Redwan Nirob <Redwan Nirob>
2021-03-26 00:24:54 +0600

    null vs undefined clear for interview

---
+++ nullVsUndefined.js
@@ -0,0 +1,15 @@
+// let pakhi;
+// function add(num1,num2){
+//     console.log(num1+num2);
+//     return num1+num2;
+// }
+
+// function add(num1,num2){
+//     console.log(num1,num2);
+// }
+
+// const premik = {name: "smart dude", phone:9875798, gf:"alia bhaatt"};
+// console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
