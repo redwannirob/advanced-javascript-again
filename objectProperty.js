ce73d34c4201619a89793c5872069c54adb388cf
Redwan Nirob <Redwan Nirob>
2021-03-28 08:08:22 +0600

    onek kisu shikhlam

---
+++ objectProperty.js
@@ -0,0 +1,18 @@
+const students = [
+    {id: 21,name: 'Omar Sani'},
+    {id: 31,name:'Manna'},
+    {id:41,name: 'Moyuri'},
+    {id:71, name:'Deepjol'}
+];
+
+const names = students.map(s => s.name);
+const ids = students.map(s => s.id);
+
+const bigger = students.filter(s=> s.id>40);
+const biggerOne = students.find(s => s.id>40);
+
+console.log(biggerOne);
+
+
+
+
